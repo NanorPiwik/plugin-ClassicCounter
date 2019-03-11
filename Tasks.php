e66578704dea539bb34ab457251c1bd8f3680f76
NanorPiwik <ronanchardonneau@gmail.com>
2019-03-11 16:23:26 +0100

    Update Tasks.php

--- Tasks.php
+++ Tasks.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-namespace Piwik\Plugins\ClassicCounter;
+namespace Piwik\Plugins\ClassicDownloadsCounter;
 
 use Piwik\Cache;
 use Piwik\FrontController;
